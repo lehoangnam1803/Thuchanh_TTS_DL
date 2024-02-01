2547034f619311a5629f29a7377735471bdfb41b
lehoangnam1803 <62412792+lehoangnam1803@users.noreply.github.com>
2024-02-01 14:38:52 +0700

    Report

--- Lab4/Lab4.m
+++ Lab4/Lab4.m
@@ -10,7 +10,6 @@ tx_data = bitxor(bit_data,r);
 
 %% Cau 3: Ma hoa qua kenh truyen nhi phan co xac suat loi = 0.2
 rx_data = bsc(tx_data,0.2);
-% Check
 [qcheck, rcheck] = deconv(rx_data,div);
 rcheck = mod(rcheck,2);
 check = sum(rcheck);
